a8717bb41d9be4d50e1fc721a9db542380189e42
Denis W. Bonaccini <114507650+bonacciniWd@users.noreply.github.com>
2023-06-26 23:13:27 -0300

    Atualizar arquivos

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
 
       
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center' style={{ marginTop: '10px' }}>
+      <div className='absolute w-full flex justify-center items-center' style={{ marginTop: '10px' }}>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
