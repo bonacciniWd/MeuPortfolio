e2e685eadfcd757d096355f4df16e200867ae194
Denis W. Bonaccini <114507650+bonacciniWd@users.noreply.github.com>
2023-06-27 01:01:04 -0300

    Atualizar arquivos

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,11 @@
 import { motion } from "framer-motion";
 import { styles } from "../styles";
-import { EarthCanvas } from "./canvas";
 
 
 const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
-      <div>
-      <EarthCanvas />
-      </div>
+      
       <div
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
