1ce848359ff522d995b5d0209c430f23ba004e50
Denis W. Bonaccini <114507650+bonacciniWd@users.noreply.github.com>
2023-06-26 23:01:19 -0300

    Atualizar arquivos

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,14 +22,16 @@ const Hero = () => {
             de usuário e aplicações web.
           </p>
           <p>
-          ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ
+          ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ
+          ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ
+          ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ
           </p>
         </div>
       </div>
 
       
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-bottom items-center'>
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
