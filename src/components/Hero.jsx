007c068309c834930dc5b4089fe92d0534cf9da9
Denis W. Bonaccini <114507650+bonacciniWd@users.noreply.github.com>
2023-10-27 20:58:45 -0300

    teste4

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,40 +19,7 @@ const Hero = () => {
     let isDeleting = false;
     const textElement = document.getElementById("animated-text");
 
-    function typeText() {
-      const currentPhrase = phrases[phraseIndex];
-      const currentText = currentPhrase.substring(0, charIndex + 1);
-      textElement.textContent = currentText;
-
-      if (!isDeleting) {
-        charIndex++;
-      } else {
-        charIndex--;
-      }
-
-      if (isDeleting && charIndex === -1) {
-        isDeleting = false;
-
-        // Aguarde 3 segundos antes de iniciar a próxima frase
-        setTimeout(() => {
-          phraseIndex = (phraseIndex + 1) % phrases.length;
-          charIndex = 0;
-          typeText();
-        }, 200);
-        return;
-      }
-
-      if (!isDeleting && charIndex === currentPhrase.length) {
-        isDeleting = true;
-      }
-
-      const speed = isDeleting ? 120 : 210; // Ajuste a velocidade de digitação e exclusão conforme necessário
-
-      setTimeout(typeText, speed);
-    }
-
-    typeText();
-  }, []);
+    
 
   return (
     <section className={`relative w-full h-screen mx-auto`}>
@@ -66,11 +33,7 @@ const Hero = () => {
            O que você  <span className='text-[#915EFF] uppercase' style={{ margin: '0 10px' }}>quer criar?</span>
           </h1>
           
-          <div className="console">
-            <pre>
-              <code id="animated-text"></code>
-            </pre>
-          </div>
+          
 
        
         <div>
